dc64a549c3e999420b7ec9669424c3b063f3d8d2
Samuel Ortega <samy9093@gmail.com>
2023-01-18 23:37:37 -0600

    Initial Commit

---
+++ DungeonDice/DungeonDiceApp.swift
@@ -0,0 +1,17 @@
+//
+//  DungeonDiceApp.swift
+//  DungeonDice
+//
+//  Created by Sammy on 1/18/23.
+//
+
+import SwiftUI
+
+@main
+struct DungeonDiceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
