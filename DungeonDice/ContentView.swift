f7ce7f0e502d5968775da91f0f30a83a6f7edfd6
Samuel Ortega <samy9093@gmail.com>
2023-01-22 17:27:45 -0600

    Start of Ch4.5 Rotation and Reflow

--- DungeonDice/ContentView.swift
+++ DungeonDice/ContentView.swift
@@ -43,16 +43,20 @@ struct ContentView: View {
             
             Spacer()
             
-            Group {
+            LazyVGrid(columns: [GridItem(.adaptive(minimum: 100, maximum: 102))]) {
                 ForEach(Dice.allCases, id: \.self) { dice in
                     Button("\(dice.rawValue)-sided") {
                         resultMessage = "You rolled a \(dice.roll()) on a \(dice.rawValue)-sided dice"
                     }
                 }
+                .buttonStyle(.borderedProminent)
+                .tint(.red)
+
             }
-            .buttonStyle(.borderedProminent)
-            .tint(.red)
-        }
+            
+            
+            }
+        
         .padding()
     }
 }
