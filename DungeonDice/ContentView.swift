bbe721a1a353f814d47bbcadba8c0dec6d718953
Samuel Ortega <samy9093@gmail.com>
2023-01-22 18:01:32 -0600

    Before Refactoring

--- DungeonDice/ContentView.swift
+++ DungeonDice/ContentView.swift
@@ -25,39 +25,78 @@ struct ContentView: View {
     }
     
     @State private var resultMessage = ""
-    
+    @State private var buttonsLeftOver = 0 //Number of buttons left over first row
+    let horizontalPadding: CGFloat = 16
+    let spacing: CGFloat = 0 //between buttons
+    let buttonWidth: CGFloat = 130
     var body: some View {
-        VStack {
-            Text("Dungeon Dice")
-                .font(.largeTitle)
-                .fontWeight(.black)
-                .foregroundColor(.red)
-            
-            Spacer()
-            
-            Text(resultMessage)
-                .font(.largeTitle)
-                .fontWeight(.medium)
-                .multilineTextAlignment(.center)
-                .frame(height: 150)
-            
-            Spacer()
-            
-            LazyVGrid(columns: [GridItem(.adaptive(minimum: 100, maximum: 102))]) {
-                ForEach(Dice.allCases, id: \.self) { dice in
-                    Button("\(dice.rawValue)-sided") {
-                        resultMessage = "You rolled a \(dice.roll()) on a \(dice.rawValue)-sided dice"
+        GeometryReader { geo in
+            VStack {
+                Text("Dungeon Dice")
+                    .font(.largeTitle)
+                    .fontWeight(.black)
+                    .foregroundColor(.red)
+                
+                Spacer()
+                
+                Text(resultMessage)
+                    .font(.largeTitle)
+                    .fontWeight(.medium)
+                    .multilineTextAlignment(.center)
+                    .frame(height: 150)
+                
+                Spacer()
+                
+                LazyVGrid(columns: [GridItem(.adaptive(minimum: buttonWidth), spacing: spacing)]) {
+                    ForEach(Dice.allCases.dropLast(buttonsLeftOver), id: \.self) { dice in
+                        Button("\(dice.rawValue)-sided") {
+                            resultMessage = "You rolled a \(dice.roll()) on a \(dice.rawValue)-sided dice"
+                        }
+                        .frame(width: buttonWidth)
                     }
-                }
-                .buttonStyle(.borderedProminent)
-                .tint(.red)
+                    .buttonStyle(.borderedProminent)
+                    .tint(.red)
 
+                }
+                HStack {
+                    ForEach(Dice.allCases.suffix(buttonsLeftOver), id: \.self) { dice in
+                        Button("\(dice.rawValue)-sided") {
+                            resultMessage = "You rolled a \(dice.roll()) on a \(dice.rawValue)-sided dice"
+                        }
+                        .frame(width: buttonWidth)
+                        
+                    }
+                    .buttonStyle(.borderedProminent)
+                    .tint(.red)
+                }
+                
+                
+                }
+            .padding()
+            .onChange(of: geo.size.width, perform: { newValue in
+                var screenWidth = geo.size.width - horizontalPadding*2 // paddin on both sides
+                if Dice.allCases.count > 1 {
+                    screenWidth += spacing
+                }
+                
+                //calculate numOfButtonsPerRow as an Int //Could drop decimal
+                let numberOfButtonsPerRow = Int(screenWidth) / Int(buttonWidth + spacing)
+                buttonsLeftOver = Dice.allCases.count % numberOfButtonsPerRow
+                print("numberOfButtonsPerRow = \(numberOfButtonsPerRow) buttonsLeftOver = \(buttonsLeftOver)")
+            })
+            .onAppear {
+                var screenWidth = geo.size.width - horizontalPadding*2 // paddin on both sides
+                if Dice.allCases.count > 1 {
+                    screenWidth += spacing
+                }
+                
+                //calculate numOfButtonsPerRow as an Int //Could drop decimal
+                let numberOfButtonsPerRow = Int(screenWidth) / Int(buttonWidth + spacing)
+                buttonsLeftOver = Dice.allCases.count % numberOfButtonsPerRow
+                print("numberOfButtonsPerRow = \(numberOfButtonsPerRow) buttonsLeftOver = \(buttonsLeftOver)")
             }
-            
-            
-            }
+        }
         
-        .padding()
     }
 }
 
