fbb1012a13692009294bf3cc25e29f1393727853
Samuel Ortega <samy9093@gmail.com>
2023-01-22 23:20:08 -0600

    Toolbars, sheets, fullscreen cover, built an interface. Starting 5.4 structs

--- DungeonDice/DetailView.swift
+++ DungeonDice/DetailView.swift
@@ -9,25 +9,71 @@ import SwiftUI
 
 struct DetailView: View {
     @Environment(\.dismiss) private var dismiss //\/dismiss is the cool thing but the var is adjustable
+    @State private var toDo = ""
+    @State private var reminderIsOn = false
+    @State private var dueDate = Date.now + (60*60*24)
+    @State private var notes = ""
+    @State private var isCompleted = false
     var passedValue: String
     
     var body: some View {
-        VStack {
-            Image(systemName: "swift")
-                .resizable()
-                .scaledToFit()
-                .foregroundColor(.orange)
-            Text("NewView!\n And you passed over the value \(passedValue)")
-                .font(.largeTitle)
-                .multilineTextAlignment(.center)
-            Spacer()
-            Button("Get Back!") {
-                dismiss() //specially treat like a function, replaces function
+        NavigationStack {
+            List {
+                TextField("Enter To Do here", text: $toDo)
+                    .font(.title)
+                    .textFieldStyle(.roundedBorder)
+                    .padding(.vertical)
+                    .listRowSeparator(.hidden)
+                //Spacer()
+                
+                Toggle("Set Reminder", isOn: $reminderIsOn)
+                    .padding(.top)
+                    .listRowSeparator(.hidden)
+                DatePicker("Date", selection: $dueDate)
+                    .listRowSeparator(.hidden)
+                    .padding(.bottom)
+                    .disabled(!reminderIsOn)
+                Text("Notes:")
+                    .padding(.top)
+                TextField("Notes", text: $notes, axis: .vertical)
+                    .textFieldStyle(.roundedBorder)
+                    .listRowSeparator(.hidden)
+                Toggle("Completed", isOn: $isCompleted)
+                    .padding(.top)
+                    .listRowSeparator(.hidden)
             }
-            .buttonStyle(.borderedProminent)
+            .listStyle(.plain)
+            .toolbar {
+                ToolbarItem(placement: .navigationBarLeading) {
+                    Button("Cancel") {
+                        dismiss() //remeber parent
+                    }
+                }
+                ToolbarItem(placement: .navigationBarTrailing) {
+                    Button("Save") {
+                         //TOD: remeber parent
+                    }
+                }
+            }
+            .navigationBarBackButtonHidden()
+            .navigationBarTitleDisplayMode(.inline)
         }
-        .padding()
-        //.navigationBarBackButtonHidden()
+        //        VStack {
+        //            Image(systemName: "swift")
+        //                .resizable()
+        //                .scaledToFit()
+        //                .foregroundColor(.orange)
+        //            Text("NewView!\n And you passed over the value \(passedValue)")
+        //                .font(.largeTitle)
+        //                .multilineTextAlignment(.center)
+        //            Spacer()
+        //            Button("Get Back!") {
+        //                dismiss() //specially treat like a function, replaces function
+        //            }
+        //            .buttonStyle(.borderedProminent)
+        //        }
+        //        .padding()
+//        .navigationBarBackButtonHidden()
     }
 }
 
--- DungeonDice/ToDoListView.swift
+++ DungeonDice/ToDoListView.swift
@@ -8,27 +8,66 @@
 import SwiftUI
 
 struct ToDoListView: View {
+   @State private var sheetIsPresented = false
+    
     var toDos = ["Learn Switft", "Build Apss", "World Piece", "Vacation"]
+    
     var body: some View {
         NavigationStack {
-            //            VStack {
-            //                NavigationLink {
-            //                    DetailView()
-            //                } label: {
-            //                    Image(systemName: "eye")
-            //                    Text("Show the New view")
-            //                }
-            //                .buttonStyle(.borderedProminent)
-            //            }
             List {
                 ForEach(toDos, id: \.self) { toDo in
-//                    Text(toDo)
+                    //                    Text(toDo)
                     NavigationLink {
                         DetailView(passedValue: toDo)
                     } label: {
                         Text(toDo)
                     }
+                    .font(.title2)
                 }
+            }
+            .navigationTitle("To Do List")
+            .navigationBarTitleDisplayMode(.automatic)
+            .listStyle(.plain)
+            .toolbar {
+                ToolbarItem(placement: .navigationBarTrailing) {
+                    Button {
+                        sheetIsPresented.toggle()
+                    } label: {
+                        Image(systemName: "plus")
+                    }
+                    //                    NavigationLink {
+//                        DetailView(passedValue: "")
+//                    } label: {
+//                        Image(systemName: "plus")
+//                    }
+                }
+                //.padding()
+            }
+//MARK: This is to present the view in a pulled up 'NewItem' action
+//            .sheet(isPresented: $sheetIsPresented) {
+//                DetailView(passedValue: "")
+//            }
+            .fullScreenCover(isPresented: $sheetIsPresented) {
+                DetailView(passedValue: "")
+            }
+        }
+    }
+    struct ToDoListView_Previews: PreviewProvider {
+        static var previews: some View {
+            ToDoListView()
+        }
+    }
+}
+
+//            VStack {
+//                NavigationLink {
+//                    DetailView()
+//                } label: {
+//                    Image(systemName: "eye")
+//                    Text("Show the New view")
+//                }
+//                .buttonStyle(.borderedProminent)
+//            }
 //                Section {
 //                    NavigationLink {
 //                        DetailView()
@@ -46,31 +85,11 @@ struct ToDoListView: View {
 //                } header: {
 //                    Text("Breaks")
 //                }
-                ForEach(1..<100, id: \.self) { number in
-                    NavigationLink {
-                        DetailView(passedValue: "Item \(number)")
-                    } label: {
-                        Text("Item \(number)")
-                    }
-
-                }
-            }
-            .navigationTitle("To Do List")
-            .navigationBarTitleDisplayMode(.automatic)
-            .listStyle(.plain)
-        }
-        
-        
-        .padding()
-    }
-    
-}
-
-
-struct ToDoListView_Previews: PreviewProvider {
-    static var previews: some View {
-        ToDoListView()
-    }
-}
-
+//                ForEach(1..<100, id: \.self) { number in
+//                    NavigationLink {
+//                        DetailView(passedValue: "Item \(number)")
+//                    } label: {
+//                        Text("Item \(number)")
+//                    }
+//                }
 
